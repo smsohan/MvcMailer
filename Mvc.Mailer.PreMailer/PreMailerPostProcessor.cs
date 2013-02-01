d757ade1b63d0e0dee177ada2ed6203293554377
Hernan Marano <herchugm@gmail.com>
2013-02-01 15:24:16 -0300

    spliting projects

---
+++ Mvc.Mailer.PreMailer/PreMailerPostProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mvc.Mailer.PreMailerPostProcessor
+{
+    public class PreMailerPostProcessor : IPostProcessor
+    {
+        public string Process(string body)
+        {
+            var pm = new PreMailer.Net.PreMailer();
+            return pm.MoveCssInline(body, false);
+        }
+    }
+}
---
+++ Mvc.Mailer/IPostProcessor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+
+
+namespace Mvc.Mailer
+{
+    public interface IPostProcessor
+    {
+        string Process(string body);
+    }
+}
